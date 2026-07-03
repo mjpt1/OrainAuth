b572a63c79e66a7c2f5d7466d599d40e5817ef09
2yt <2yt.dev.con@gmail.com>
2026-07-03 20:17:45 +0430

    Project files (Django default)

---
+++ OrainAuth/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from dashboard import views
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('admin/', admin.site.urls, name='admin'),
+    path('dashboard', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')),
+]
